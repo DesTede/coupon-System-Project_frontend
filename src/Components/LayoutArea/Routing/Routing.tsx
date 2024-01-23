99174682e5ee9f2de78b0da9d2ee6c67b314e7db
Des <desaleytede@gmail.com>
2024-01-23 18:53:16 +0200

    Components changes: added LayoutArea and more

--- src/Components/LayoutArea/Routing/Routing.tsx
+++ src/Components/LayoutArea/Routing/Routing.tsx
@@ -4,22 +4,33 @@ import HomePage from "../../MainArea/HomePage/HomePage";
 import Coupons from "../../CouponArea/Coupons/Coupons";
 import Login from "../../AuthArea/Login/Login";
 import NotFound from "../NotFound/NotFound";
+import Companies from "../../CompanyArea/Companies/Companies";
+import CompanyCard from "../../CompanyArea/CompanyCard/CompanyCard";
+import Customers from "../../CustomerArea/Customers/Customers";
+import CustomerCard from "../../CustomerArea/CustomerCard/CustomerCard";
+import UpdateCompany from "../../CompanyArea/UpdateCompany/UpdateCompany";
+import UpdateCustomer from "../../CustomerArea/UpdateCustomer/UpdateCustomer";
+import AddCompany from "../../CompanyArea/AddCompany/AddCompany";
+import AddCustomer from "../../CustomerArea/AddCustomer/AddCustomer";
 
 function Routing(): JSX.Element {
     return (
         <div className="Routing">
 			<Routes>
                 <Route path={"home"} element={<HomePage/>} />
-                <Route path={"login"} element={<Login/>}/>
-                <Route/>
                 <Route path={"coupons"} element={<Coupons/>}/>
-                <Route/>
-                <Route/>
-                <Route/>
-                <Route/>
-                <Route/>
-                <Route/>
-                <Route/>
+                <Route path={"login"} element={<Login/>}/>
+                
+                <Route path={"companies"} element={<Companies/>}/>
+                <Route path={"company/companyid"} element={<CompanyCard/>}/>
+                <Route path={"company/add"} element={<AddCompany/>}/>
+                <Route path={"company/update/companyid"} element={<UpdateCompany/>}/>
+                
+                <Route path={"customers"} element={<Customers/>}/>
+                <Route path={"customer/customerid"} element={<CustomerCard/>}/>
+                <Route path={"customer/add"} element={<AddCustomer/>}/>
+                <Route path={"customer/update/customerid"} element={<UpdateCustomer/>}/>
+                
                 <Route/>
                 <Route/>
                 <Route path={"/"} element={<Navigate to={"home"}/> }/>
